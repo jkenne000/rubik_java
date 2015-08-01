c58bfdc11ae6b4adbf40abf94a73a00a45c4368b
jkenne000 <jd_kennedy@cable.comcast.com>
2015-08-01 12:49:22 -0700

    played with code

--- rubik/RubiksCube.java
+++ rubik/RubiksCube.java
@@ -1,16 +1,15 @@
-import java.io.Console;
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.Vector;
+package rubik;
+
 import java.util.Iterator;
 import java.util.Random;
-import java.lang.Math;
+import java.util.Vector;
 
 public class RubiksCube {
  
-    private int homeCount;
-    private int maxHomeCubeCount;
-    private Vector cubeVector = new Vector();
+    private static int maxHomeCubeCount;
+    private static int homeCubeCount;
+    
+    private Vector<Cube> cubeVector = new Vector<Cube>();
     private Random rand = new Random();
     int i;
     int count;
@@ -74,70 +73,91 @@ public class RubiksCube {
     }
 
 
-    private void tryRotation(Vector cubeVector, int axis, boolean polarity, boolean clockwise) 
-	throws Exception {
-	int count = rotate(cubeVector, axis, polarity, clockwise);
-	rotate(cubeVector, axis, polarity, clockwise ? false : true);
-	if (count > maxHomeCubeCount) {
-	    maxHomeCubeCount = count;
-	    bestAxis = axis;
-	    bestPolarity = polarity;
-	    bestClockwise = clockwise;
+	private void tryRotation(Vector<Cube> cubeVector, int axis, boolean polarity,
+			boolean clockwise, int rescursionCount) throws Exception {
+		int count = rotate(cubeVector, axis, polarity, clockwise);
+		rotate(cubeVector, axis, polarity, clockwise ? false : true);
+		if (count > maxHomeCubeCount) {
+			maxHomeCubeCount = count;
+			bestAxis = axis;
+			bestPolarity = polarity;
+			bestClockwise = clockwise;
+		}
+		System.out.println("rescursionCount = " + rescursionCount);
+		rescursionCount--;
+		if (rescursionCount > 0) {
+			tryRotation(cubeVector,  axis,  polarity,
+			 clockwise,  rescursionCount);
+		}
 	}
-    }
 
-    private int rotate(Vector cubeVector, int axis, boolean polarity, boolean clockwise) throws Exception {
-	Iterator it = cubeVector.iterator();
-	boolean solved = true;
-	    
-	int homeCubeCount = 0;
-	//System.out.println("axis = " + axis + " pol = " + polarity + " cw = " + clockwise);
-	while (it.hasNext()) {
-	    Cube c = (Cube)it.next();
-	    //c.dump();
-	    if (c.rotate(axis, polarity, clockwise)) {
-		homeCubeCount++;
-	    }
-	    else {
-		solved = false;
-	    }
-	}
-	//System.out.println("home cube count = " + homeCubeCount);
-	return homeCubeCount;
-    }
+	private int rotate(Vector<Cube> cubeVector, int axis, boolean polarity,
+			boolean clockwise) throws Exception {
+		Iterator<Cube> it = cubeVector.iterator();
+		boolean solved = true;
 
-    private boolean analyze(Vector cubeVector) throws Exception {
-	Iterator it = cubeVector.iterator();
-	boolean solved = true;
-	    
-	int homeCubeCount = 0;
-	while (it.hasNext()) {
-	    Cube c = (Cube)it.next();
-	    if (c.isHome()) {
-		homeCubeCount++;
-	    }
+		int homeCubeCount = 0;
+		// System.out.println("axis = " + axis + " pol = " + polarity + " cw = "
+		// + clockwise);
+		while (it.hasNext()) {
+			Cube c = (Cube) it.next();
+			// c.dump();
+			if (c.rotate(axis, polarity, clockwise)) {
+				homeCubeCount++;
+			} else {
+				solved = false;
+			}
+		}
+		// System.out.println("home cube count = " + homeCubeCount);
+		return homeCubeCount;
 	}
-	System.out.println("home cube count = " + homeCubeCount);
-	return solved;
-    }
 
-    private void solve() throws Exception {
-	System.out.println("solving");
-	analyze(cubeVector);
-	maxHomeCubeCount = 0;
-	for (axis = 0; axis < 3; axis++) {
-	    tryRotation(cubeVector, axis, false, false);
-	    tryRotation(cubeVector, axis, false, true);
-	    tryRotation(cubeVector, axis, true, false);
-	    tryRotation(cubeVector, axis, true, true);
+	private boolean analyze(Vector<Cube> cubeVector) throws Exception {
+		Iterator<Cube> it = cubeVector.iterator();
+		boolean solved = true;
+
+	    homeCubeCount = 0;
+		while (it.hasNext()) {
+			Cube c = (Cube) it.next();
+			if (c.isHome()) {
+				homeCubeCount++;
+			}
+		}
+		System.out.println("home cube count = " + homeCubeCount);
+		System.out.println("maxHomeCubeCount = " + maxHomeCubeCount);
+		if (homeCubeCount == 26) {
+			System.out.println("SOLVED!!");
+		}
+		return solved;
 	}
-	analyze(cubeVector);
-    }
+
+	private void solve() throws Exception {
+		System.out.println("trying all rotations...");
+		for (axis = 0; axis < 3; axis++) {
+			tryRotation(cubeVector, axis, false, false, 1);
+			tryRotation(cubeVector, axis, false, true, 1);
+			tryRotation(cubeVector, axis, true, false, 1);
+			tryRotation(cubeVector, axis, true, true, 1);
+		}
+		analyze(cubeVector);
+		rotate(cubeVector, this.bestAxis, this.bestPolarity, this.bestClockwise);
+
+		if (maxHomeCubeCount <= homeCubeCount) {
+			Thread.sleep(3000L);
+			System.out.println("recursing");
+			solve();
+		}
+	}
 
     public static void main(String args[]) throws Exception {
 	RubiksCube rc = new RubiksCube();
+	maxHomeCubeCount = 0;
+
 	try {
-	    rc.solve();
+			while (true) {
+				rc.solve();
+				Thread.sleep(3000L);
+			}
 	} catch (Exception e) {
 	    System.out.println("Exception " + e);
 	}
